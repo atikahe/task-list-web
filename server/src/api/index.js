ec62b8ebc7a31f3b0876b1d0c3e7e858751f92e0
Atikah <Atikah@aia.com>
2020-09-20 16:02:27 +0700

    chore(init): bootstrap server

---
+++ server/src/api/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const emojis = require('./emojis');
+
+const task = require('./task');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    message: 'API - 👋🌎🌍🌏'
+  });
+});
+
+router.use('/emojis', emojis);
+router.use('/task', task);
+
+module.exports = router;
